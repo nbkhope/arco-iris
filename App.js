37e36efab61765b1fd01b4e19740f554f295e617
nbkhope <ryumemaru@yahoo.com>
2017-08-20 18:41:57 -0700

    Terminar projeto (move lista de cores para estado inicial do componente)

--- App.js
+++ App.js
@@ -2,17 +2,33 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: [
+        'red',
+        '#ff7f00',
+        'yellow',
+        'rgb(0, 255, 0)',
+        'blue',
+        '#4b0082',
+        '#8b00ff',
+      ],
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      const lastColor = this.state.colors.slice(-1);
+      const listWithoutLast = this.state.colors.slice(0, -1);
+
+      this.setState({ colors: lastColor.concat(listWithoutLast) });
+    }, 1000);
+  }
+
   render() {
-    const colors = [
-      'red',
-      '#ff7f00',
-      'yellow',
-      'rgb(0, 255, 0)',
-      'blue',
-      '#4b0082',
-      '#8b00ff',
-    ];
-    const views = colors.map(color => (
+    const views = this.state.colors.map(color => (
       <View key={color} style={{ flex: 1, backgroundColor: color }} />
     ));
 
